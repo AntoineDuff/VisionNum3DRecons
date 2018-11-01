236d78dd5bb9a8f644418a139ed458064f5fca2e
Antoine Dufour <antoine@MacBook-Pro-de-Antoine.local>
2018-11-01 12:34:00 -0400

    wip

--- camera_calib.py
+++ camera_calib.py
@@ -2,14 +2,13 @@ import numpy as np
 import cv2
 import glob
 
-def calibrate_camera():
+def calibrate_camera(i_grid_size, j_grid_size):
 
     # termination criteria
     criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
-    # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-    objp = np.zeros((9*6,3), np.float32)
-    objp[:,:2] = np.mgrid[0:6,0:9].T.reshape(-1,2)
+    objp = np.zeros((i_grid_size * j_grid_size, 3), np.float32)
+    objp[:,:2] = np.mgrid[0:i_grid_size, 0:j_grid_size].T.reshape(-1,2)
 
     # Arrays to store object points and image points from all the images.
     objpoints = [] # 3d point in real world space
@@ -22,18 +21,18 @@ def calibrate_camera():
         gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
         # Find the chess board corners
-        ret, corners = cv2.findChessboardCorners(gray, (6,9),None)
+        ret, corners = cv2.findChessboardCorners(gray, (i_grid_size, j_grid_size),None)
 
         # If found, add object points, image points (after refining them)
         if ret == True:
             objpoints.append(objp)
 
-            corners2 = cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
+            corners2 = cv2.cornerSubPix(gray,corners,(11, 11),(-1, -1), criteria)
             imgpoints.append(corners2)
 
             # Draw and display the corners
-            img = cv2.drawChessboardCorners(img, (6,9), corners2,ret)
-            ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1],None,None)
+            img = cv2.drawChessboardCorners(img, (i_grid_size, j_grid_size), corners2, ret)
+            ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
             cv2.imshow('img',img)
             cv2.waitKey(10000)
     cv2.destroyAllWindows()
--- main.py
+++ main.py
@@ -1,2 +1,5 @@
+from camera_calib import calibrate_camera
+
 if __name__ == '__main__':
+    calibrate_camera(6, 9)
     print(0)
