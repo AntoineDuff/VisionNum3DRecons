15a7fd0d611794cd97a5c300175b52112720d17b
Antoine Dufour <antoine@MBP-de-Antoine.local>
2018-11-17 12:12:53 -0500

    wip

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
 from camera_calib import calibrate_camera
 import cv2
+import numpy as np
+import os.path
 
 if __name__ == '__main__':
     
-    (ret_A,
-    camera_matrix_A,
-    distortion_coefficients_A,
-    rotation_vecs_A,
-    translation_vecs_A) = calibrate_camera(6, 9, './calibA/*.jpg')
 
-    (ret_B,
-    camera_matrix_B,
-    distortion_coefficients_B,
-    rotation_vecs_B,
-    translation_vecs_B) = calibrate_camera(6, 9, './calibB/*.jpg')
+    if (os.path.isfile("camera_matrix_A.npy") == False):
+        (ret_A,
+        camera_matrix_A,
+        distortion_coefficients_A,
+        rotation_vecs_A,
+        translation_vecs_A) = calibrate_camera(6, 9, './calibA/*.jpg')
+
+        np.save("camera_matrix_A",camera_matrix_A)
+        np.save("rotation_vecs_A", rotation_vecs_A)
+        np.save("translation_vecs_B", translation_vecs_A)
+        print("calibration A done!")
+
+
+    # (ret_B,
+    # camera_matrix_B,
+    # distortion_coefficients_B,
+    # rotation_vecs_B,
+    # translation_vecs_B) = calibrate_camera(6, 9, './calibB/*.jpg')
 
     # cam = cv2.VideoCapture(0)
 
